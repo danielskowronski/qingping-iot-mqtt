9f2f86f1b6fe8ae52c71b77b7ada0f35a3180433
Daniel Skowroński <daniel@skowron.ski>
2025-11-15 19:36:40 +0100

    hatch new

---
+++ src/qingping_iot_mqtt/cli/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2025-present Daniel Skowroński <daniel@skowron.ski>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+import click
+
+from qingping_iot_mqtt.__about__ import __version__
+
+
+@click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
+@click.version_option(version=__version__, prog_name="qingping-iot-mqtt")
+def qingping_iot_mqtt():
+    click.echo("Hello world!")
